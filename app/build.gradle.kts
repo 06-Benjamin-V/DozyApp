899c97a9c401b7eff6f2dd5d9836ed6494278999
Benjamin <b.valenzuela06@ufromail.cl>
2025-04-19 09:37:16 -0400

    se agregar dependencias de firebase y el archivo json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,4 +38,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    //Dependencias Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+
 }
